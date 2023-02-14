841306a6886c60b7613e01cec93ebf63c6f4c318
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-02-14 09:46:06 +0100

    UTRG-7:Grid HTML placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarModule } from 'primeng/menubar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabViewModule } from 'primeng/tabview';
-
-import { NavigationItemsProviderService } from './services/navigation-items-provider.service';
 
 import { MenubarComponent } from './header/menubar/menubar.component';
 import { BannerComponent } from './header/banner/banner.component';
@@ -23,6 +22,7 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     MenubarModule,
     TabViewModule,
