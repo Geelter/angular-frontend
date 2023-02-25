11aecd883486c4ba9e4bac538473a90b947c3dbe
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-02-25 15:14:15 +0100

    UTRG-13:Generate the AuthContainerComponent files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '@core/core.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from '@auth/auth.module';
+import { AuthContainerComponent } from './features/auth/auth-container/auth-container/auth-container.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AuthContainerComponent],
   imports: [CoreModule, AuthModule],
   providers: [],
   exports: [],
