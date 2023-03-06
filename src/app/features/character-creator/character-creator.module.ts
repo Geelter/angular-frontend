af63a8401c43a6408060f34761718fd553c98d8b
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-06 17:50:01 +0100

    UTRG-12:Move navigation back to main component

--- src/app/features/character-creator/character-creator.module.ts
+++ src/app/features/character-creator/character-creator.module.ts
@@ -4,13 +4,9 @@ import { SharedModule } from '@shared/shared.module';
 
 import { CharacterCreatorContainerComponent } from './components/character-creator-container/character-creator-container.component';
 import { CharacterCreatorRoutingModule } from '@creator/character-creator-routing.module';
-import { CharacterCreatorNavComponent } from './components/character-creator-nav/character-creator-nav.component';
 
 @NgModule({
-  declarations: [
-    CharacterCreatorContainerComponent,
-    CharacterCreatorNavComponent,
-  ],
+  declarations: [CharacterCreatorContainerComponent],
   imports: [CharacterCreatorRoutingModule, CommonModule, SharedModule],
   exports: [CharacterCreatorContainerComponent],
 })
--- src/app/features/character-creator/components/character-creator-nav/character-creator-nav.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-character-creator-nav',
-  templateUrl: './character-creator-nav.component.html',
-  styleUrls: ['./character-creator-nav.component.scss'],
-})
-export class CharacterCreatorNavComponent {}
