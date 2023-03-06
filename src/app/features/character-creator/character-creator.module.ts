b6a2bbf5c022120d7393ecb85350fa37c17ef088
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-06 12:41:33 +0100

    UTRG-12:Create temporary test layout

--- src/app/features/character-creator/character-creator.module.ts
+++ src/app/features/character-creator/character-creator.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 
 import { CharacterCreatorContainerComponent } from './components/character-creator-container/character-creator-container.component';
 
 @NgModule({
   declarations: [CharacterCreatorContainerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
+  exports: [CharacterCreatorContainerComponent],
 })
 export class CharacterCreatorModule {}
