e49bbc29e69c542b4cb5a91b91c92d0b2c218e6a
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-07 20:14:03 +0100

    UTRG-12:Add routing for CharacterCreatorName component

--- src/app/features/character-creator/character-creator-routing.module.ts
+++ src/app/features/character-creator/character-creator-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CharacterCreatorContainerComponent } from '@creator/components/character-creator-container/character-creator-container.component';
+import { CharacterCreatorArchetypeComponent } from '@creator/components/steps/character-creator-archetype/character-creator-archetype.component';
+import { CharacterCreatorNameComponent } from '@creator/components/steps/character-creator-name/character-creator-name.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CharacterCreatorContainerComponent,
+    children: [
+      {
+        path: 'archetype',
+        component: CharacterCreatorArchetypeComponent,
+      },
+      {
+        path: 'name',
+        component: CharacterCreatorNameComponent,
+      },
+    ],
   },
 ];
 @NgModule({
