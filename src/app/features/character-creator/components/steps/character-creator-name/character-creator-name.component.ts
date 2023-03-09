9bec5b039c901f34a7a4de5f1e0db18e94c3694e
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-09 13:55:09 +0100

    UTRG-12:Add methods for navigation and managing the step data

--- src/app/features/character-creator/components/steps/character-creator-name/character-creator-name.component.ts
+++ src/app/features/character-creator/components/steps/character-creator-name/character-creator-name.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CharacterCreatorService } from '@creator/character-creator.service';
 
 @Component({
   selector: 'app-character-creator-name',
   templateUrl: './character-creator-name.component.html',
   styleUrls: ['./character-creator-name.component.scss'],
 })
-export class CharacterCreatorNameComponent {}
+export class CharacterCreatorNameComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private creatorService: CharacterCreatorService
+  ) {}
+  characterName: string;
+
+  previousStep() {
+    this.router.navigate(['/creator', 'archetype']);
+  }
+
+  submitCharacter() {
+    this.creatorService.submitCharacter();
+  }
+
+  saveNameToService(name: string) {
+    this.creatorService.characterName = name;
+  }
+
+  ngOnInit() {
+    this.characterName = this.creatorService.characterName;
+  }
+}
