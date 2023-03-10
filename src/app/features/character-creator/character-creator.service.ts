f442545d97cfaff2f6bb51a96f3c88eb83b97fce
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-10 10:28:06 +0100

    UTRG-12:Save character data to supabase in submitCharacter

--- src/app/features/character-creator/character-creator.service.ts
+++ src/app/features/character-creator/character-creator.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 import { dummyArchetypes } from '@assets/dummy-data/dummyArchetypes';
+import { SupabaseService } from '@core/services/supabase.service';
+import { SupabaseAuthService } from '@core/services/supabase-auth.service';
 
 @Injectable()
 export class CharacterCreatorService {
+  constructor(
+    private supabase: SupabaseService,
+    private supabaseAuth: SupabaseAuthService
+  ) {}
   chosenArchetypeIndex = 0;
 
   characterName = '';
@@ -17,12 +23,19 @@ export class CharacterCreatorService {
     return this.archetypes[this.chosenArchetypeIndex];
   }
 
-  submitCharacter() {
-    if (this.characterDataComplete) {
-      console.log(
-        'Chosen archetype is ' + dummyArchetypes[this.chosenArchetypeIndex].name
-      );
-      console.log('Chosen character name is ' + this.characterName);
+  async submitCharacter() {
+    const userID = await this.supabaseAuth.getUserID();
+    if (this.characterDataComplete && userID) {
+      console.log(userID);
+      const { data, error } = await this.supabase.client
+        .from('player_character')
+        .insert({
+          user_id: userID,
+          name: this.characterName,
+        })
+        .select();
+      console.log(data);
+      console.log(error);
     } else {
       console.log('Missing required character data');
     }
