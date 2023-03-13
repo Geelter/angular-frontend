ab4eb48e9c45680503d368f4764ae635dbef2ac0
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-13 16:02:21 +0100

    UTRG-12:Replace StepDataResolver with StepDataGuard

---
+++ src/app/features/character-creator/step-data.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Archetype } from '@assets/dummy-data/dummyArchetypes';
+import { CharacterCreatorService } from '@creator/character-creator.service';
+
+export interface StepData {
+  archetypes: Archetype[];
+}
+
+@Injectable()
+export class StepDataGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private creatorService: CharacterCreatorService
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+    if (this.creatorService.stepData) {
+      return true;
+    } else {
+      this.creatorService.fetchStepData();
+    }
+
+    if (!this.creatorService.stepData) {
+      return this.router.parseUrl('/');
+    }
+
+    return true;
+  }
+}
