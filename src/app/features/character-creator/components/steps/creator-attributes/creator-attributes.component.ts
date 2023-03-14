c0850d192f5ae32df029221b9696394038d0d199
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-14 13:21:22 +0100

    UTRG-12:Generate files for CreatorAttributes component

---
+++ src/app/features/character-creator/components/steps/creator-attributes/creator-attributes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creator-attributes',
+  templateUrl: './creator-attributes.component.html',
+  styleUrls: ['./creator-attributes.component.scss'],
+})
+export class CreatorAttributesComponent {}
