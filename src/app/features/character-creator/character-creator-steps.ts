237afa8dfa2ed08e2f19f3c1c3748dadfa4f85f4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-14 14:40:26 +0100

    UTRG-12:Create a constant holding character creator related router paths

---
+++ src/app/features/character-creator/character-creator-steps.ts
@@ -0,0 +1,9 @@
+const ROOT_PATH = '/';
+const BASE_CREATOR_PATH = '/creator';
+export const characterCreatorSteps: string[][] = [
+  [ROOT_PATH],
+  [BASE_CREATOR_PATH, 'archetype'],
+  [BASE_CREATOR_PATH, 'attributes'],
+  [BASE_CREATOR_PATH, 'name'],
+  [ROOT_PATH],
+];
