992b9997722e6832751ce7d5e69b546f6780622f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-03-21 15:36:15 +0100

    UTRG-12:Add Categories component to PostsModule

--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoriesComponent],
   imports: [CommonModule, SharedModule],
 })
 export class PostsModule {}
