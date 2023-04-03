d5c04be65669cb62a84b55152c44bf438c6a3bde
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-03 13:35:23 +0200

    UTRG-12:Add route for PostsContainerComponent as parent route

--- src/app/features/posts/posts-routing.module.ts
+++ src/app/features/posts/posts-routing.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { ThreadsComponent } from '@posts/components/threads/threads.component';
+import { PostsContainerComponent } from '@posts/components/posts-container/posts-container.component';
 
 const routes: Routes = [
   {
-    path: 'categories',
-    component: CategoriesComponent,
-  },
-  {
-    path: 'threads',
-    component: ThreadsComponent,
+    path: '',
+    component: PostsContainerComponent,
+    children: [
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: 'threads',
+        component: ThreadsComponent,
+      },
+    ],
   },
 ];
 @NgModule({
