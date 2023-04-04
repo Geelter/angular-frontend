20443d7194f7a3f4d243264fe1c49c62b06ce225
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-04 10:05:24 +0200

    UTRG-15:Functionality for fetching post categories on component init with priority for cached data

--- src/app/features/posts/components/categories/categories.component.ts
+++ src/app/features/posts/components/categories/categories.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { dummyCategories } from '@assets/dummy-data/posts';
+import { PostsService } from '@posts/posts.service';
+import { OnInit } from '@angular/core';
+import { Category } from '@assets/dummy-data/posts';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {
-  categories = dummyCategories;
+export class CategoriesComponent implements OnInit {
+  constructor(public postsService: PostsService) {}
+  categories: Promise<Category[]>;
+
+  ngOnInit() {
+    if (this.postsService.categoriesCached()) {
+      this.categories = new Promise<Category[]>(resolve => {
+        resolve(this.postsService.getSavedCategories());
+      });
+    } else {
+      this.categories = this.postsService.fetchPostCategories();
+    }
+  }
 }
