0a59500ee2d1d46c2a031a3c7163d9cbb333aa64
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-04 09:55:45 +0200

    UTRG-15:Module imports

--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { PostsRoutingModule } from '@posts/posts-routing.module';
+import { ThreadsComponent } from './components/threads/threads.component';
+import { PostsService } from '@posts/posts.service';
+import { PostsContainerComponent } from './components/posts-container/posts-container.component';
 
 @NgModule({
-  declarations: [CategoriesComponent],
-  imports: [CommonModule, SharedModule],
+  providers: [PostsService],
+  declarations: [
+    CategoriesComponent,
+    ThreadsComponent,
+    PostsContainerComponent,
+  ],
+  imports: [CommonModule, PostsRoutingModule, SharedModule],
+  exports: [CategoriesComponent],
 })
 export class PostsModule {}
