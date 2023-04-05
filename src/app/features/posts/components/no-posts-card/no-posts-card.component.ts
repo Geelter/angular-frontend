693fd51ee8b96d4e73fe3f7df999fe7af5c86923
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-05 17:24:39 +0200

    UTRG-15:Generate files for NoPostsCardComponent

---
+++ src/app/features/posts/components/no-posts-card/no-posts-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-posts-card',
+  templateUrl: './no-posts-card.component.html',
+  styleUrls: ['./no-posts-card.component.scss'],
+})
+export class NoPostsCardComponent {}
