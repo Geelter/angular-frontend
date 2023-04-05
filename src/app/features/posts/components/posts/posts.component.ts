6aa7be046fcf38f31d02eab85fee54e11c498c28
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-05 17:17:08 +0200

    UTRG-15:Replicate ngOnInit behaviour from categories and threads components

--- src/app/features/posts/components/posts/posts.component.ts
+++ src/app/features/posts/components/posts/posts.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '@posts/posts.service';
+import { Post } from '@assets/dummy-data/posts';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {}
+export class PostsComponent implements OnInit {
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
+
+  posts: Promise<Post[]>;
+
+  postsAreLoading: boolean;
+
+  ngOnInit() {
+    let threadID = '';
+
+    this.postsAreLoading = true;
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      threadID = params.get('thread_id')!;
+    });
+
+    this.posts = this.postsService.getPostsForThreadID(threadID).finally(() => {
+      this.postsAreLoading = false;
+    });
+  }
+}
