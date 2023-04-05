5b10fa8681d73c330283692e11de0524901f21a0
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-05 18:52:44 +0200

    UTRG-15:Add styling for NoPostsCardComponent

--- src/app/features/posts/components/no-posts-card/no-posts-card.component.ts
+++ src/app/features/posts/components/no-posts-card/no-posts-card.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-no-posts-card',
   templateUrl: './no-posts-card.component.html',
   styleUrls: ['./no-posts-card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NoPostsCardComponent {}
