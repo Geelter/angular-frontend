0ea2bd007d181f0e3fc097b2e2ec427dfd35d02b
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-04-21 12:59:07 +0200

    UTRG-15:Call fetch methods on init

--- src/app/features/character-creator/components/steps/creator-attributes/creator-attributes.component.ts
+++ src/app/features/character-creator/components/steps/creator-attributes/creator-attributes.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CharacterCreatorService } from '@creator/character-creator.service';
+import { AttributesConfig } from '@creator/models/attributes-config';
+import { Attribute } from '@creator/models/attribute';
 
 @Component({
   selector: 'app-creator-attributes',
   templateUrl: './creator-attributes.component.html',
   styleUrls: ['./creator-attributes.component.scss'],
 })
-export class CreatorAttributesComponent {
+export class CreatorAttributesComponent implements OnInit {
   constructor(
     private router: Router,
     public creatorService: CharacterCreatorService
@@ -28,5 +30,10 @@ export class CreatorAttributesComponent {
 
   nextStep() {
     this.router.navigate(this.nextStepRoute);
+  }
+
+  ngOnInit() {
+    this.attributesConfig = this.creatorService.getAttributesConfig();
+    this.characterAttributes = this.creatorService.getAttributes();
   }
 }
