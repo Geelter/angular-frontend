009da31da4a7bd3b14b15c6b18eb987535652e40
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-12 00:39:37 +0200

    UTRG-17:Make selectors more granular for finer control over state retrieval

--- src/app/core/store/selectors/posts/post-categories.selectors.ts
+++ src/app/core/store/selectors/posts/post-categories.selectors.ts
@@ -1,25 +1,94 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { PostCategoriesState } from '@core/store/state/posts/post-categories.state';
 import { adapter } from '@core/store/reducers/posts/post-categories.reducer';
+import { EntityPaginationState } from '@core/store/state/pagination.state';
+import { Category } from '@posts/models/category';
 
-export const featureKey = 'postCategories';
+const featureKey = 'postCategories';
 
-export const selectPostCategoriesState =
+const selectPostCategoriesState =
   createFeatureSelector<PostCategoriesState>(featureKey);
 
+export const selectCategoriesIsLoading = createSelector(
+  selectPostCategoriesState,
+  (categoriesState: PostCategoriesState) => categoriesState.isLoading
+);
+
+const selectCategories = createSelector(
+  selectPostCategoriesState,
+  (categoriesState: PostCategoriesState) => categoriesState.categories
+);
+
+const selectCategoryIDs = createSelector(
+  selectPostCategoriesState,
+  (categoriesState: PostCategoriesState) => categoriesState.categoryIDs
+);
+
 export const {
-  selectIds: selectCategoryIDs,
+  selectIds: selectCategoryEntityIDs,
   selectEntities: selectCategoryEntities,
   selectAll: selectAllCategories,
   selectTotal: selectTotalCategories,
-} = adapter.getSelectors(selectPostCategoriesState);
+} = adapter.getSelectors(selectCategories);
 
-export const selectPostCategoriesFetchDate = createSelector(
+export const selectCategoriesPaginationState = createSelector(
   selectPostCategoriesState,
-  (state: PostCategoriesState) => state.lastFetchedAt
+  (categoriesState: PostCategoriesState) => categoriesState.pagination
 );
 
-export const selectPostCategoriesIsLoading = createSelector(
-  selectPostCategoriesState,
-  (state: PostCategoriesState) => state.isLoading
+/*
+  If this returns 'null' fetch the category IDs
+ */
+const selectCategoryIDsForCurrentPage = createSelector(
+  selectCategoryIDs,
+  selectCategoriesPaginationState,
+  (categoryIDs: number[] | null, paginationState: EntityPaginationState) => {
+    if (!categoryIDs) return null;
+
+    const pageSize = paginationState.pageSize;
+    const currentPage = paginationState.currentPage;
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = currentPage * pageSize - 1;
+
+    return categoryIDs.slice(startIndex, endIndex);
+  }
+);
+
+/*
+  If this return 'null' fetch the category IDs, if it returns
+  a non-empty array fetch categories for those IDs
+*/
+export const selectMissingCategoryIDs = createSelector(
+  selectCategoryEntityIDs,
+  selectCategoryIDsForCurrentPage,
+  (
+    entityCategoryIDs: string[] | number[],
+    currentPageCategoryIDs: number[] | null
+  ) => {
+    if (!currentPageCategoryIDs) return null;
+
+    const entityCategoryIDsAsNumbers = (
+      entityCategoryIDs as Array<number | string>
+    ).map(Number);
+
+    return currentPageCategoryIDs.filter(
+      id => !entityCategoryIDsAsNumbers.includes(id)
+    );
+  }
+);
+
+/*
+  If this returns 'null' fetch category IDs
+ */
+export const selectCategoriesWithIDsForCurrentPage = createSelector(
+  selectAllCategories,
+  selectCategoryIDsForCurrentPage,
+  (categories: Category[], currentPageCategoryIDs: number[] | null) => {
+    if (!currentPageCategoryIDs) return null;
+
+    return categories.filter(category =>
+      currentPageCategoryIDs.includes(category.id)
+    );
+  }
 );
--- src/app/core/store/selectors/posts/post-threads.selectors.ts
+++ src/app/core/store/selectors/posts/post-threads.selectors.ts
@@ -1,30 +1,100 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { PostThreadsState } from '@core/store/state/posts/post-threads.state';
-import { adapter } from '@core/store/reducers/posts/post-threads.reducer';
+import {
+  CategoryThreadsState,
+  PostThreadsState,
+} from '@core/store/state/posts/post-threads.state';
+import {
+  adapter,
+  initialCategoryThreadsState,
+} from '@core/store/reducers/posts/post-threads.reducer';
+import { Thread } from '@posts/models/thread';
 
 export const featureKey = 'postThreads';
 
-export const selectPostThreadsState =
+const selectPostThreadsState =
   createFeatureSelector<PostThreadsState>(featureKey);
 
-export const {
-  selectIds: selectThreadIDs,
-  selectEntities: selectThreadEntities,
-  selectAll: selectAllThreads,
-  selectTotal: selectTotalThreads,
-} = adapter.getSelectors(selectPostThreadsState);
-
-export const selectThreadsByCategory = (categoryID: number) =>
-  createSelector(selectAllThreads, threads =>
-    threads.filter(thread => thread.category_id === categoryID)
-  );
-
-export const selectPostThreadsFetchDate = createSelector(
+export const selectThreadsIsLoading = createSelector(
   selectPostThreadsState,
-  (state: PostThreadsState) => state.lastFetchedAt
+  (threadsState: PostThreadsState) => threadsState.isLoading
 );
 
-export const selectPostThreadsIsLoading = createSelector(
+const selectCategoryThreadsStateForChosenCategory = createSelector(
   selectPostThreadsState,
-  (state: PostThreadsState) => state.isLoading
+  (threadsState: PostThreadsState) =>
+    threadsState.categoryThreads[threadsState.chosenCategory] ??
+    initialCategoryThreadsState
+);
+
+const selectThreadsForChosenCategory = createSelector(
+  selectCategoryThreadsStateForChosenCategory,
+  (categoryThreadsState: CategoryThreadsState) => categoryThreadsState.threads
+);
+
+export const {
+  selectIds: selectThreadIDsForChosenCategory,
+  selectEntities: selectThreadEntities,
+  selectAll: selectAllThreadsForChosenCategory,
+  selectTotal: selectTotalThreads,
+} = adapter.getSelectors(selectThreadsForChosenCategory);
+
+export const selectPaginationStateForChosenCategory = createSelector(
+  selectCategoryThreadsStateForChosenCategory,
+  (categoryThreadsState: CategoryThreadsState) =>
+    categoryThreadsState.pagination
+);
+
+/*
+  If this returns 'null' fetch the thread IDs for the category
+ */
+const selectIDsForCurrentPage = createSelector(
+  selectCategoryThreadsStateForChosenCategory,
+  (categoryThreadsState: CategoryThreadsState) => {
+    if (!categoryThreadsState.threadIDs) return null;
+
+    const threadIDs = categoryThreadsState.threadIDs;
+    const pageSize = categoryThreadsState.pagination.pageSize;
+    const currentPage = categoryThreadsState.pagination.currentPage;
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = currentPage * pageSize - 1;
+
+    return threadIDs.slice(startIndex, endIndex);
+  }
+);
+
+/*
+  If this return 'null' fetch the thread IDs for the category, if it returns
+  a non-empty array fetch threads for those IDs
+*/
+export const selectMissingThreadIDs = createSelector(
+  selectThreadIDsForChosenCategory,
+  selectIDsForCurrentPage,
+  (
+    threadIDsForChosenCategory: number[] | string[],
+    currentPageThreadIDs: number[] | null
+  ) => {
+    if (!currentPageThreadIDs) return null;
+
+    const threadIDsForCategoryAsNumbers = (
+      threadIDsForChosenCategory as Array<number | string>
+    ).map(Number);
+
+    return currentPageThreadIDs.filter(
+      id => !threadIDsForCategoryAsNumbers.includes(id)
+    );
+  }
+);
+
+/*
+  If this returns 'null' fetch thread IDs for the category
+ */
+export const selectThreadsWithIDsForCurrentPage = createSelector(
+  selectAllThreadsForChosenCategory,
+  selectIDsForCurrentPage,
+  (threads: Thread[], currentPageThreadIDs: number[] | null) => {
+    if (!currentPageThreadIDs) return null;
+
+    return threads.filter(thread => currentPageThreadIDs.includes(thread.id));
+  }
 );
