8c20bcef66436015aff27a50e83cffe3d90e3ee4
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-15 19:35:20 +0200

    UTRG-17:Rewrite both auth form to reactive forms

--- src/app/features/auth/login-form/login-form.component.ts
+++ src/app/features/auth/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SupabaseAuthService } from '@core/services/supabase/supabase-auth.service';
 import { NavigationService } from '@core/services/navigation.service';
 
@@ -8,8 +8,14 @@ import { NavigationService } from '@core/services/navigation.service';
   templateUrl: './login-form.component.html',
   styleUrls: ['../auth-container/auth-container.component.scss'],
 })
-export class LoginFormComponent {
-  @ViewChild('loginForm') loginForm: NgForm;
+export class LoginFormComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private supabaseAuth: SupabaseAuthService,
+    private navigationService: NavigationService
+  ) {}
+
+  loginForm: FormGroup;
 
   isSubmitting = false;
 
@@ -28,8 +34,11 @@ export class LoginFormComponent {
     }
   }
 
-  constructor(
-    private supabaseAuth: SupabaseAuthService,
-    private navigationService: NavigationService
-  ) {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      rememberMe: [false],
+    });
+  }
 }
--- src/app/features/auth/register-form/register-form.component.ts
+++ src/app/features/auth/register-form/register-form.component.ts
@@ -1,5 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { SupabaseAuthService } from '@core/services/supabase/supabase-auth.service';
 import { NavigationService } from '@core/services/navigation.service';
 
@@ -8,17 +15,23 @@ import { NavigationService } from '@core/services/navigation.service';
   templateUrl: './register-form.component.html',
   styleUrls: ['../auth-container/auth-container.component.scss'],
 })
-export class RegisterFormComponent {
-  @ViewChild('registerForm') registerForm: NgForm;
+export class RegisterFormComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private supabaseAuth: SupabaseAuthService,
+    private navigationService: NavigationService
+  ) {}
+
+  registerForm: FormGroup;
 
   isSubmitting = false;
 
   async onSubmit() {
     if (this.registerForm.valid) {
-      const { email, password } = this.registerForm.value;
+      const { username, email, password } = this.registerForm.value;
       this.isSubmitting = true;
 
-      const data = await this.supabaseAuth.register(email, password);
+      const data = await this.supabaseAuth.register(username, email, password);
 
       if (data) {
         await this.navigationService.navigateToRoot();
@@ -28,8 +41,46 @@ export class RegisterFormComponent {
     }
   }
 
-  constructor(
-    private supabaseAuth: SupabaseAuthService,
-    private navigationService: NavigationService
-  ) {}
+  passwordMatchValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    if (control && control instanceof FormGroup) {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+
+      return password &&
+        confirmPassword &&
+        password.value === confirmPassword.value
+        ? null
+        : { passwordMismatch: true };
+    }
+    return null;
+  };
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(4)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(22),
+          ],
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(22),
+          ],
+        ],
+        terms: [false, Validators.requiredTrue],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
 }
