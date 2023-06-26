3b9e1e7c15db9ff5d0b4cf12f968694cce1b68d2
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-27 01:40:47 +0200

    UTRG-17:Create functionality for dynamically determining the number of layout columns based on window width.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { SupabaseAuthService } from '@core/services/supabase/supabase-auth.service';
 import { Router } from '@angular/router';
@@ -8,12 +8,39 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private primengConfig: PrimeNGConfig,
     private supabaseAuth: SupabaseAuthService,
     private router: Router
   ) {}
+
+  resizeListener: (_: UIEvent) => void;
+
+  private handleResize(event: UIEvent) {
+    this.setColumnCount(window.innerWidth);
+  }
+
+  private setColumnCount(windowWidth: number) {
+    let columnCount;
+
+    if (windowWidth < 600) {
+      columnCount = 2;
+    } else if (windowWidth < 900) {
+      columnCount = 3;
+    } else {
+      columnCount = 4;
+    }
+
+    document.documentElement.style.setProperty(
+      '--column-count',
+      columnCount.toString()
+    );
+
+    // console.log(
+    //   document.documentElement.style.getPropertyValue('--column-count')
+    // );
+  }
 
   redirectIfLoggedIn() {
     this.supabaseAuth.getSession().then(result => {
@@ -25,6 +52,17 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.primengConfig.ripple = true;
 
+    this.resizeListener = this.handleResize.bind(this);
+    window.addEventListener('resize', this.resizeListener);
+    this.setColumnCount(window.innerWidth);
+
     // this.redirectIfLoggedIn();
+
+    // Change this to an action dispatch
+    // this.archetypesService.fetchArchetypeDescriptions();
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.resizeListener);
   }
 }
