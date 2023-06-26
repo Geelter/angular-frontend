aac1164fb7fcffc73ab6d819326a62ef62e583e7
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-27 01:20:29 +0200

    UTRG-17:File deletions

--- src/app/core/store/state/player-characters.state.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerCharacter } from '@creator/models/player-character.model';
-import { EntityState } from '@ngrx/entity';
-
-export interface PlayerCharactersState extends EntityState<PlayerCharacter> {
-  currentUserCharacterID: number | null;
-
-  chosenCharacterID: number | null;
-}
