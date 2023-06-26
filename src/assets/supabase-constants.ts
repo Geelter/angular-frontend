4c21da52e69e07ab5061684aa1f78cb2d1d6df4f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-27 01:35:35 +0200

    UTRG-17:Add further constants for Supabase database tables

--- src/assets/supabase-constants.ts
+++ src/assets/supabase-constants.ts
@@ -1,10 +1,18 @@
-export const PLAYER_CHARACTER_TABLE = 'player_character';
+export const PROFILES_TABLE = 'profiles';
 
 export const POST_CATEGORY_TABLE = 'post_category';
 
 export const POST_THREAD_TABLE = 'post_thread';
 
 export const POST_TABLE = 'post';
+
+export const ARCHETYPE_DESCRIPTION_TABLE = 'archetype_description';
+
+export const ATTRIBUTE_DESCRIPTION_TABLE = 'attribute_description';
+
+export const ATTRIBUTE_ALLOCATION_CONFIG_TABLE = 'attribute_allocation_config';
+
+export const PLAYER_ATTRIBUTE_TABLE = 'player_attribute';
 
 export const DEFAULT_DATA_LIFESPAN = 300000;
 
