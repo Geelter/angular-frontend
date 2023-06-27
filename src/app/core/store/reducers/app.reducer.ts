f15fa056859f34b929b1eae9028b43a244df7367
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2023-06-27 16:35:48 +0200

    UTRG-17:Add profile and archetype to app state/reducer map

--- src/app/core/store/reducers/app.reducer.ts
+++ src/app/core/store/reducers/app.reducer.ts
@@ -1,14 +1,17 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import { AppState } from '@core/store/state/app.state';
-import { playerCharactersReducer } from './player-characters.reducer';
+import { profilesStateReducer } from './profiles.reducer';
 import { postCategoriesReducer } from '@core/store/reducers/posts/post-categories.reducer';
 import { postThreadsReducer } from '@core/store/reducers/posts/post-threads.reducer';
 import { postsReducer } from '@core/store/reducers/posts/posts.reducer';
 import { globalReducer } from '@core/store/reducers/global.reducer';
+import { archetypesStateReducer } from '@core/store/reducers/archetypes.reducer';
 
 export const appReducer: ActionReducerMap<AppState> = {
-  playerCharacters: playerCharactersReducer,
+  profiles: profilesStateReducer,
+
+  archetypes: archetypesStateReducer,
 
   postCategories: postCategoriesReducer,
 
--- src/app/core/store/state/app.state.ts
+++ src/app/core/store/state/app.state.ts
@@ -1,11 +1,13 @@
-import { PlayerCharactersState } from '@core/store/state/player-characters.state';
+import { ProfilesState } from '@core/store/state/profiles.state';
 import { PostCategoriesState } from '@core/store/state/posts/post-categories.state';
 import { PostThreadsState } from '@core/store/state/posts/post-threads.state';
 import { PostsState } from '@core/store/state/posts/posts.state';
 import { GlobalState } from '@core/store/state/global.state';
+import { ArchetypesState } from '@core/store/state/archetypes.state';
 
 export interface AppState {
-  playerCharacters: PlayerCharactersState;
+  profiles: ProfilesState;
+  archetypes: ArchetypesState;
   postCategories: PostCategoriesState;
   postThreads: PostThreadsState;
   posts: PostsState;
